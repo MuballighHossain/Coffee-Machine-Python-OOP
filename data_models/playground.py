c4a7f6232e0be2762dbcdd984039228838826dee
muballighhossain <mubahossain99@gmail.com>
2023-05-02 23:03:11 +0600

    2/5/23

--- data_models/playground.py
+++ data_models/playground.py
@@ -1,4 +1,6 @@
 from data_models.AquaPokemon import AquaPokemon
+from data_models.FirePokemon import FirePokemon
 
 squirtle = AquaPokemon('squirtle', '002', 10, 50, 10, 20, [('tackle', 5)])
+charmander = FirePokemon('charmander', '003', 15, 55, 15, 25, [('tackle', 10)])
 
